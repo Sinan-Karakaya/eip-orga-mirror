33c9e0f3be24f96e0e815ea5235f6e6e8bcec68c
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-02 16:23:05 +0900

    fix bad push

--- mirror.sh
+++ mirror.sh
@@ -10,9 +10,11 @@ git remote add mirror "$INPUT_TARGET_REPO_URL"
 
 mkdir -p .$INPUT_TARGET_SUBDIRECTORY
 mv * .$INPUT_TARGET_SUBDIRECTORY
+mv .$INPUT_TARGET_SUBDIRECTORY/.git .
+mv .$INPUT_TARGET_SUBDIRECTORY/.github .
 mv .$INPUT_TARGET_SUBDIRECTORY $INPUT_TARGET_SUBDIRECTORY
 
-git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
+git push --tags --force --prune mirror "refs/remotes/origin/$INPUT_TARGET_SUBDIRECTORY/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
