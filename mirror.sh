a625992b402623b1ad0a54d7eebb06fa9501feb6
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-02 14:12:52 +0900

    test: target_subdir

--- mirror.sh
+++ mirror.sh
@@ -5,6 +5,12 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
+
+temp_name=$(uuidgen)
+mkdir -p $temp_name
+mv * $temp_name
+mv $temp_name $INPUT_TARGET_SUBDIRECTORY
+
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
