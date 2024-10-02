8cafe7e3b2e00cab4bf899809493b7c4d39a453a
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-02 15:42:19 +0900

    fix temp_name

--- mirror.sh
+++ mirror.sh
@@ -8,7 +8,7 @@ sh -c "git config --global --add safe.directory $PWD"
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 
-temp_name=$(uuidgen)
+temp_name=5387F78C      # Random name
 mkdir -p $temp_name
 mv * $temp_name
 mv $temp_name $INPUT_TARGET_SUBDIRECTORY
