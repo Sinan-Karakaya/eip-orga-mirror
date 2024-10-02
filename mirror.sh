c49e3d8953d5703decfee92e71afd7efc1a3cc98
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-02 16:26:30 +0900

    utfiytfyit

--- mirror.sh
+++ mirror.sh
@@ -11,8 +11,6 @@ git remote add mirror "$INPUT_TARGET_REPO_URL"
 mkdir -p .$INPUT_TARGET_SUBDIRECTORY
 mv * .$INPUT_TARGET_SUBDIRECTORY
 mv .$INPUT_TARGET_SUBDIRECTORY $INPUT_TARGET_SUBDIRECTORY
-mv $INPUT_TARGET_SUBDIRECTORY/.git .
-mv $INPUT_TARGET_SUBDIRECTORY/.github .
 
 git push --tags --force --prune mirror "refs/remotes/origin/$INPUT_TARGET_SUBDIRECTORY/*:refs/heads/*"
 
