7b4b91dd3231d2c4a39259c9f3fdf190920ce5e9
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-02 16:18:16 +0900

    fix ssh key format

--- mirror.sh
+++ mirror.sh
@@ -5,7 +5,7 @@ sh -c "git config --global --add safe.directory $PWD"
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 
 mkdir -p .$INPUT_TARGET_SUBDIRECTORY
